058d59f48526c3d3f6ac3b2e2d050fbc23b90566
klphillips <klphillips1206@gmail.com>
2017-09-08 00:52:14 +0000

    adding first test of hw2.sql

--- hw2.sql
+++ hw2.sql
@@ -4,6 +4,8 @@
    Assignment: Homework Assignment #2 
 */
 
+--Part (2) Creating the table Countrie 
+
 CREATE TABLE Countries (
 name VARCHAR(20),
 latitude INT,
@@ -14,11 +16,15 @@ gdp INT,
 gdpYear INT,
 PRIMARY KEY (name));
 
+--Part (3) Adding Primary Key
+--Part (4a) Added the Borders table!
+
 CREATE TABLE Borders (
 name VARCHAR(20),
 borderCountries VARCHAR(100),
 PRIMARY KEY (name));
 
+--Part (4b) Populate both tables with the necessary information from the world factbook 
 
 INSERT INTO Countries VALUES('Austria', 4720, 1320, 83871, 8754413, 47900, 2016);
 INSERT INTO Countries VALUES ('Belgium',5050,400, 30528, 11491346, 44900, 2016);
@@ -30,7 +36,7 @@ INSERT INTO Countries VALUES('Netherlands', 5230, 545, 41543, 17084719, 50800, 2
 INSERT INTO Countries VALUES('Poland',5200, 2000, 312685, 38476269, 27700, 2016);
 INSERT INTO Countries VALUES('Switzerland', 4700, 800, 41277, 8236303, 59400, 2016);
 
-INSERT INTO Borders VALUES ('Germany', 'Netherlands, Belgium, Luxemburg, Poland, Czechia, Austria, France, Switzerland)';
+INSERT INTO Borders VALUES ('Germany', 'Netherlands, Belgium, Luxemburg, Poland, Czechia, Austria, France, Switzerland');
 INSERT INTO Borders VALUES ('Netherlands', 'Belgium, Germany');
 INSERT INTO Borders VALUES ('Belgium', 'Netherlands, Germany, Luxemburg, France');
 INSERT INTO Borders VALUES ('Luxemburg', 'Germany, Belgium, France');
@@ -40,8 +46,9 @@ INSERT INTO Borders VALUES ('Austria', 'Czechia, Switzerland, Germany');
 INSERT INTO Borders VALUES ('France', 'Belgium, Luxemburg, Germany, Switzerland');
 INSERT INTO Borders VALUES ('Switzerland', 'France, Germany, Austria');
 
+--Part(4c) Query to list border countries with Germany
 SELECT * FROM Borders WHERE name='Germany';
-
-SELECT * FROM Countries WHERE population>10000000;
-
+--Part(4d) Query the list of countries with a population greater than 35000000
+SELECT Countries.name FROM Countries WHERE population>35000000;
+--Part(4e) Query to find countries with a population greater than 35000000 that also border Germany
 SELECT Countries.name, Borders.borderCountries FROM Countries, Borders WHERE(Countries.population>35000000) AND (Borders.name = 'Germany');
