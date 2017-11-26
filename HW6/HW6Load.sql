b68a9b098032dca0945daf5432518e700b2ea3ac
klphillips <31387691+klphillips@users.noreply.github.com>
2017-11-26 12:20:54 -0700

    Committing file that creates and loads information for HW6

---
+++ HW6/HW6Load.sql
@@ -0,0 +1,146 @@
+/* Author: Kyra Phillips
+   Due Date: 10/26/2017
+   Help from/Worked with: Myself
+   Assignment: Homework Assignment #5
+*/
+
+/*This table is specfically for the customers or purchasers of the tickets.
+I did this because you cannot always assume that the people buying are the people flying.
+In addition, there could be several passengers and only one payer.
+*/
+
+CREATE TABLE Payers(
+	Email VARCHAR(200),
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
+	PRIMARY KEY(Email)
+);
+
+--This table is specifically for holding all of the information that goes with a phone number of a payer.
+
+CREATE TABLE PayerPhoneNumbers(
+	countryCode INT,
+	localCode INT, 
+	localNumber INT,
+	Email VARCHAR(200),
+	FOREIGN KEY(Email) REFERENCES Payer(Email)
+);
+
+-- This table is for all of the information needed for a Payers address
+
+CREATE TABLE PayerAddress(
+	payerCountry VARCHAR(25),
+	payerCity VARCHAR(25),
+	payerState VARCHAR(25),
+	payerPostalCode INT,
+	payerStreetAddress VARCHAR(200),
+	Email VARCHAR(200),
+	FOREIGN KEY(Email) REFERENCES Payer(Email)
+);
+
+/* This table is the information needed for each individual passenger.
+I made this table because Payers and passengers could be different and there could be several passengers per payer.
+In addition, I added an extra field for a primary key of a unique passenger ID.
+*/
+
+CREATE TABLE Passenger(
+	payerEmail VARCHAR(200),
+	bookingNumber INT,
+	uniqueFlightNumber INT,
+	passFirstName VARCHAR(50),
+	passLastName VARCHAR(50),
+	passEmail VARCHAR(200),
+	passID INT,
+	PRIMARY KEY(passID),
+	FOREIGN KEY(payerEmail) REFERENCES Payers(Email),
+	FOREIGN KEY(bookingNumber) REFERENCES Booking(bookingNumber),
+	FOREIGN KEY(uniqueFlightNumber) REFERENCES Flights(uniqueFlightNumber)
+);
+
+--This table is specifically for holding all of the information that goes with a phone number of a passenger.
+
+CREATE TABLE PassengerPhoneNumbers(
+	countryCode INT,
+	localCode INT, 
+	localNumber INT,
+	Email VARCHAR(200),
+	FOREIGN KEY(Email) REFERENCES Payer(Email)
+);
+
+-- This table is for all of the information needed for a Passengers address
+
+CREATE TABLE PassengerAddress(
+	passCountry VARCHAR(25),
+	passCity VARCHAR(25),
+	passState VARCHAR(25),
+	passPostalCode INT,
+	passStreetAddress VARCHAR(200),
+	Email VARCHAR(200),
+	FOREIGN KEY(Email) REFERENCES Payer(Email)
+);
+
+-- This table is for airlines and airline information
+
+CREATE TABLE Airlines(
+	airlineName VARCHAR(200),
+	airlineCode VARCHAR(10),
+	airlineCountry VARCHAR(25),
+	PRIMARY KEY(airlineCode)
+);
+
+--This table is the information needed for each individual flight
+
+CREATE TABLE Flights(
+	uniqueFlightNumber INT,
+	originCode INT,
+	destinationCode INT, 
+	airlineCode VARCHAR(10),
+	length INT,
+	departureDate VARCHAR(50),
+	departureHour INT,
+	departureMinute INT,
+	arrivalDate VARCHAR(50),
+	arrivalHour INT,
+	arrivalMin INT,
+	PRIMARY KEY (uniqueFlightNumber),
+	FOREIGN KEY (airlineCode) REFERENCES Airlines(airlineCode),
+	FOREIGN KEY (originCode) REFERENCES Airports(airportCode),
+	FOREIGN KEY (destinationCode) REFERENCES Airports(airportCode)
+);
+
+CREATE TABLE Airports(
+	airportCode INT,
+	airportName VARCHAR(50),
+	cityName VARCHAR(50),
+	stateName VARCHAR(50),
+	PRIMARY KEY (airportCode)
+);
+
+--This table is the necessary information for each booking
+
+CREATE TABLE Booking(
+	bookingNumber INT,
+	payerEmail VARCHAR(200),
+	payerFirstName VARCHAR(50),
+	payerLastName VARCHAR(50),
+	bookingCityCode INT,
+	bookingDate VARCHAR(25),
+	numberOfPassengers INT,
+	PRIMARY KEY(bookingNumber),
+	FOREIGN KEY(payerEmail) REFERENCES Payers(Email)
+);
+
+/* I made this table to keep track of people on a plane.
+That way there is a roster of passengers with their booking information for each flight.
+*/
+
+CREATE TABLE IndividualFlight(
+	uniqueFlightNumber INT,
+	bookingNumber INT,
+	passID INT,
+	FOREIGN KEY(uniqueFlightNumber) REFERENCES Flights(uniqueFlightNumber),
+	FOREIGN KEY(bookingNumber) REFERENCES Booking(bookingNumber),
+	FOREIGN KEY(passID) REFERENCES Passenger(passID)
+);
+
+COPY Flights FROM '.\Flights.csv' DELIMITER ',' CSV HEADER;
