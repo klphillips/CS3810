e52c9bc99704a166234d5935e39c370ab962e3c7
klphillips <31387691+klphillips@users.noreply.github.com>
2017-12-03 23:11:01 -0700

    Updating final edition of HW3Load.sql

--- HW3/HW3Load.sql
+++ HW3/HW3Load.sql
@@ -1,59 +1,27 @@
 /* Author: Kyra Phillips
-   Due Date: 10/8/2017 - resubmitting 10/24/2017
-   Help from/Worked with: Myself, Cooper Stevens, Max Morrison, Jack Kolb
+   Due Date: 10/8/2017 - resubmitting 12/3/2017
+   Help from/Worked with: Myself, Cooper Stevens
    Assignment: Homework Assignment #3
 */
 
 -- Copy SQL Statements, part two to HW3
 
-Copy Carriers (UNIQUE_CARRIER, UNIQUE_CARRIER_ENTITY, UNIQUE_CARRIER_NAME)
-from './Carriers.csv' DELIMITER ',' CSV HEADER;
+COPY Airline (AIRLINE_ID, CARRIER, CARRIER_ENTITY, CARRIER_NAME, UNIQUE_CARRIER, UNIQUE_CARRIER_ENTITY, UNIQUE_CARRIER_NAME, WAC, CARRIER_GROUP, CARRIER_GROUP_NEW, REGION, START_DATE_SOURCE, THRU_DATE_SOURCE) from '.\Airline.csv' DELIMITER ',' CSV HEADER;  
 
-Copy CarrierEntity (UNIQUE_CARRIER_ENTITY, CARRIER_GROUP, CARRIER_GROUP_NEW, REGION, AIRLINE_ID)
-from './CarrierEntity.csv' DELIMITER ',' CSV HEADER;
+COPY Carrier (UNIQUE_CARRIER_ENTITY, WAC, CARRIER_GROUP) from '.\CARRIER.csv' DELIMITER ',' CSV HEADER;  
 
-Copy CarrierEntityDates (START_DATE_SOURCE, THRU_DATE_SOURCE, UNIQUE_CARRIER_ENTITY)
-from './'
+COPY UniqueCarrierName (UNIQUE_CARRIER, UNIQUE_CARRIER_NAME) from '.\UniqueCarrierName.csv' DELIMITER ',' CSV HEADER; 
 
-Copy Origins (ORIGIN_STATE_NM, ORIGIN_AIRPORT_ID, ORIGIN)
-from './Origins.csv' DELIMITER ',' CSV HEADER;
+COPY Airport (AIRPORT_ID, AIRPORT_SEQ_ID, CITY_MARKET_ID, AIRPORT_WAC, AIRPORT_NAME) from '.\Airport.csv' DELIMITER ',' CSV HEADER; 
 
-Copy OriginNames (ORIGIN_STATE_NM, ORIGIN_WAC, ORIGIN_STATE_FIPS, ORIGIN_STATE_ABR)
-from './OriginNames.csv' DELIMITER ',' CSV HEADER;
+COPY Cities (CITY_MARKET_ID, CITY_NAME, STATE_FIPS) from '.\Cities.csv' DELIMITER ',' CSV HEADER;  
 
-Copy OriginAirports (ORIGIN_AIRPORT_SEQ_ID, ORIGIN_AIRPORT_ID, ORIGIN_CITY_MARKET_ID)
-from './OriginAirports.csv' DELIMITER ',' CSV HEADER;
+COPY States (STATE_FIPS, STATE_NAME, STATE) from '.\States.csv' DELIMITER ',' CSV HEADER; 
 
-Copy OriginCityName (ORIGIN_CITY_NAME, ORIGIN)
-from './OriginCityName.csv' DELIMITER ',' CSV HEADER;
+COPY Passengers (UNIQUE_CARRIER_ENTITY, ORIGIN_AIRPORT_ID, DEST_AIRPORT_ID, PASSENGERS) from '.\Passengers.csv' DELIMITER ',' CSV HEADER; 
 
-Copy Destinations (DEST_STATE_NM, DEST_AIRPORT_ID, DEST)
-from './Destinations.csv' DELIMITER ',' CSV HEADER;
+COPY Freight (UNIQUE_CARRIER_ENTITY, ORIGIN_AIRPORT_ID, DEST_AIRPORT_ID, FREIGHT) from '.\Freight.csv' DELIMITER ',' CSV HEADER; 
 
-Copy DestinationAirport (DEST_AIRPORT_ID, DEST_AIRPORT_SEQ_ID, DEST_CITY_MARKET_ID)
-from './DestinationAirport.csv' DELIMITER ',' CSV HEADER;
+COPY Mail (UNIQUE_CARRIER_ENTITY, ORIGIN_AIRPORT_ID, DEST_AIRPORT_ID, MAIL) from '.\Mail.csv' DELIMITER ',' CSV HEADER; 
 
-Copy DestinationNames (DEST_STATE_ABR, DEST_STATE_FIPS, DEST_STATE_NM, DEST_WAC)
-from './DestinationNames.csv' DELIMITER ',' CSV HEADER;
-
-Copy DestinationCityNames (DEST, DEST_CITY_NAME)
-from './DestinationCityNames.csv' DELIMITER ',' CSV HEADER;
-
-Copy Aircrafts (AIRCRAFT_GROUP, AIRCRAFT_TYPE, AIRCRAFT_CONFIG, CLASS, DISTANCE, DEPARTURES_SCHEDULED, DEPARTURES_PERFORMED)
-from './Aircrafts.csv' DELIMITER ',' CSV HEADER;
-
-Copy AircraftDistance (DISTANCE_GROUP, DISTANCE)
-from './AircraftDistance.csv' DELIMITER ',' CSV HEADER;
-
-Copy AircraftSeatsPassengers (AIRCRAFT_CONFIG, SEATS, PASSENGERS)
-from './AircraftSeatsPassengers.csv' DELIMITER ',' CSV HEADER;
-
-Copy Airtime (RAMP_TO_RAMP, AIR_TIME, DISTANCE)
-from './Airtime.csv' DELIMITER ',' CSV HEADER;
-
-Copy TimePeriod (YEAR, QUARTER, MONTH)
-from './TimePeriod.csv' DELIMITER ',' CSV HEADER;
-
-Copy Payload (PAYLOAD, FREIGHT, MAIL)
-from './Payload.csv' DELIMITER ',' CSV HEADER;
-
+COPY FLIGHT(FLIGHT_NUMBER, DISTANCE, AIR_TIME, FREIGHT, PASSENGERS, ORIGIN_STATE_ABR, DEST_STATE_ABR, UNIQUE_CARRIER_ENTITY) from '.\Flights.csv' DELIMITER ',' CSV HEADER;
